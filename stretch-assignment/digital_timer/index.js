bb52a9878d7aa91a09e8920265ab3d7ca92ade60
Ronald Libago <code@ronaldlibago.com>
2019-04-06 17:39:52 -0700

    Revert "Stretch: Red text when timer finishes."
    This reverts commit b74c5061a585e47974ac8532f417a1a617b806aa.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,16 +1,11 @@
 const secondTens = document.querySelector("#secondTens");
 const secondOnes = document.querySelector("#secondOnes");
-const colon = document.querySelector("#colon");
 const msHundreds = document.querySelector("#msHundreds");
 const msTens = document.querySelector("#msTens");
-
 let mSeconds = 0;
 
 const beginCount = setInterval(() => {
-    if (mSeconds === 999) { 
-        clearInterval(beginCount); 
-        applyColor("red");
-    }
+    if (mSeconds === 999) { clearInterval(beginCount); }
     mSeconds++;
     secondTens.innerText = mSeconds.toString().slice(-4,-3) || `-`;
     secondOnes.innerText = mSeconds.toString().slice(-3,-2) || `-`;
@@ -18,12 +13,6 @@ const beginCount = setInterval(() => {
     msTens.innerText = mSeconds % 10;
 }, 10);
 
-function applyColor(color) {
-    let digitPlaces = [secondTens, secondOnes, colon, msHundreds, msTens];
-    for (let place of digitPlaces) {
-        place.style.color = color;
-    }
-}
 // setTimeout(() => clearInterval(beginCount), 12500);
 
 // How to use `clearInterval()` after a set amount of time
